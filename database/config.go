b1001c332847f65096ebddbb0f2795bdbbe58879
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-03 16:46:40 +0200

    Refactor database service and enhance validation.
    Replaced inline error messages with constants for reusability and consistency. Refactored `Service` to include initialization checks, DSN generator, and enhanced config validation. Added new dependencies, interfaces, and helper methods to support database operations.

--- database/config.go
+++ database/config.go
@@ -1,15 +1,15 @@
 package database
 
 type Config struct {
-	Driver          string `json:"driver" validate:"required"`
+	Driver          string `json:"driver" validate:"required,oneof=postgres sqlite3"`
 	Host            string `json:"host" validate:"required,hostname|ip"`
 	Port            int    `json:"port" validate:"required,min=1,max=65535"`
 	User            string `json:"user" validate:"required,min=1,max=63,alphanum|contains=_|contains=-"`
 	Password        string `json:"pass" validate:"required,min=1"`
 	Database        string `json:"database" validate:"required,min=1,max=63,alphanum|contains=_|contains=-"`
 	SSLMode         string `json:"ssl_mode" validate:"required,oneof=disable require verify-ca verify-full"`
-	MaxOpenConns    int    `json:"max_open_conns" validate:"required"`
-	MaxIdleConns    int    `json:"max_idle_conns" validate:"required"`
+	MaxOpenConns    int    `json:"max_open_conns" validate:"required,min=1"`
+	MaxIdleConns    int    `json:"max_idle_conns" validate:"required,min=1"`
 	ConnMaxLifetime int    `json:"conn_max_lifetime" validate:"required"` // Number of seconds
 	Debug           bool   // Enable SQLBoiler query logging
 }
