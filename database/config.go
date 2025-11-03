cdf9c2f1e1bdfd3b517bfb3208471367b283459a
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-03 15:29:37 +0200

    Expand `Config` struct with detailed database connection parameters and validation tags

--- database/config.go
+++ database/config.go
@@ -2,7 +2,12 @@ package database
 
 type Config struct {
 	Driver          string `json:"driver" validate:"required"`
-	DSN             string `json:"dsn" validate:"required"`
+	Host            string `json:"host" validate:"required,hostname|ip"`
+	Port            int    `json:"port" validate:"required,min=1,max=65535"`
+	User            string `json:"user" validate:"required,min=1,max=63,alphanum|contains=_|contains=-"`
+	Password        string `json:"pass" validate:"required,min=1"`
+	Database        string `json:"database" validate:"required,min=1,max=63,alphanum|contains=_|contains=-"`
+	SSLMode         string `json:"ssl_mode" validate:"required,oneof=disable require verify-ca verify-full"`
 	MaxOpenConns    int    `json:"max_open_conns" validate:"required"`
 	MaxIdleConns    int    `json:"max_idle_conns" validate:"required"`
 	ConnMaxLifetime int    `json:"conn_max_lifetime" validate:"required"` // Number of seconds
