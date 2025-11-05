bc497e111ca636f57baf0be2d0b4b1a974015e26
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-05 09:03:30 +0200

    Enhance `LoggingConfig` with JSON tags and validation rules
    Added JSON serialization tags and validation rules to `LoggingConfig` fields to improve input validation and ensure consistency in logging configuration.

--- logging.go
+++ logging.go
@@ -3,13 +3,13 @@ package types
 const LoggerServiceName = "logger"
 
 type LoggingConfig struct {
-	Level             string
-	SkipFrameCount    int
-	WithTimestamp     bool
-	ConsoleLogging    bool
-	FileLogging       bool
-	RelLogFileDir     string
-	LogFileMaxBackups int
-	LogFileMaxAgeDays int
-	LogFileMaxSizeMB  int
+	Level             string `json:"level" validate:"oneof=debug info warn error fatal panic"`
+	SkipFrameCount    int    `json:"skip_frame_count" validate:"min=0"`
+	WithTimestamp     bool   `json:"with_timestamp"`
+	ConsoleLogging    bool   `json:"console_logging"`
+	FileLogging       bool   `json:"file_logging"`
+	RelLogFileDir     string `json:"rel_log_file_dir" validate:"dir"`
+	LogFileMaxBackups int    `json:"log_file_max_backups" validate:"min=1"`
+	LogFileMaxAgeDays int    `json:"log_file_max_age_days" validate:"min=7"`
+	LogFileMaxSizeMB  int    `json:"log_file_max_size_mb" validate:"min=10"`
 }
