ca19366a52fb1e73093a2bfdad3e548ed4bc4575
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-05 14:04:06 +0200

    Refine `DatastoreConfig` validation rules and update `.gitignore`
    Improved validation for `DatastoreConfig` fields, introducing conditional rules based on the `Driver` type. Enhanced flexibility with optional validations for unused configurations. Updated `.gitignore` to exclude test-related files.

--- datastore.go
+++ datastore.go
@@ -6,24 +6,21 @@ const (
 )
 
 type DatastoreConfig struct {
-	Driver          string `json:"driver" validate:"required,oneof=postgres sqlite"`
-	Path            string `json:"path" validate:"required_if=Driver sqlite"` // Used for sqlite3 only
-	Options         string `json:"options"`                                   // Used for sqlite3 only
-	Host            string `json:"host" validate:"required,hostname|ip"`
-	Port            int    `json:"port" validate:"required,min=1,max=65535"`
-	User            string `json:"user" validate:"required,min=1,max=63,alphanum|contains=_|contains=-"`
-	Password        string `json:"pass" validate:"required,min=1"`
-	Database        string `json:"database" validate:"required,min=1,max=63,alphanum|contains=_|contains=-"`
-	SSLMode         string `json:"ssl_mode" validate:"required,oneof=disable require verify-ca verify-full"`
-	MaxOpenConns    int    `json:"max_open_conns" validate:"required,min=1"`
-	MaxIdleConns    int    `json:"max_idle_conns" validate:"required,min=1"`
-	ConnMaxLifetime int    `json:"conn_max_lifetime" validate:"required,min=0"`  // Number of minutes: 1-15 minutes
-	ConnMaxIdleTime int    `json:"conn_max_idle_time" validate:"required,min=0"` // Number of minutes: 1-5 minutes
-
-	ContextTimeout            int `json:"context_timeout" validate:"required,min=5"`             // Number of seconds, with a minimum of 5 seconds
-	TransactionContextTimeout int `json:"transaction_context_timeout" validate:"required,min=5"` // Number of seconds, with a minimum of 5 seconds
-
-	Debug bool // Enable SQLBoiler query logging
-
-	Params map[string]string `json:"params"`
+	Driver                    string            `json:"driver" validate:"oneof=postgres sqlite"`
+	Path                      string            `json:"path" validate:"required_if=Driver sqlite,omitempty"` // Used for sqlite3 only
+	Options                   string            `json:"options" validate:"omitempty"`                        // Used for sqlite3 only
+	Host                      string            `json:"host" validate:"required_if=Driver postgres,omitempty,hostname|ip"`
+	Port                      int               `json:"port" validate:"required_if=Driver postgres,omitempty,min=1,max=65535"`
+	User                      string            `json:"user" validate:"required_if=Driver postgres,omitempty,min=1,max=63,alphanum|contains=_|contains=-"`
+	Password                  string            `json:"pass" validate:"required_if=Driver postgres,omitempty,min=1"`
+	Database                  string            `json:"database" validate:"required_if=Driver postgres,omitempty,min=1,max=63,alphanum|contains=_|contains=-"`
+	SSLMode                   string            `json:"ssl_mode" validate:"required_if=Driver postgres,omitempty,oneof=disable require verify-ca verify-full"`
+	MaxOpenConns              int               `json:"max_open_conns" validate:"required,min=1"`
+	MaxIdleConns              int               `json:"max_idle_conns" validate:"required,min=1"`
+	ConnMaxLifetime           int               `json:"conn_max_lifetime" validate:"required,min=0"`           // Number of minutes: 1-15 minutes
+	ConnMaxIdleTime           int               `json:"conn_max_idle_time" validate:"required,min=0"`          // Number of minutes: 1-5 minutes
+	ContextTimeout            int               `json:"context_timeout" validate:"required,min=5"`             // Number of seconds, with a minimum of 5 seconds
+	TransactionContextTimeout int               `json:"transaction_context_timeout" validate:"required,min=5"` // Number of seconds, with a minimum of 5 seconds
+	Debug                     bool              // Enable SQLBoiler query logging
+	Params                    map[string]string `json:"params" validate:"omitempty"`
 }
