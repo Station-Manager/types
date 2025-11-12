715b294b47d58f3678364d40bbe09b7887517e8a
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-12 14:07:32 +0200

    Refine validation for `LogFileMaxSizeMB` by adding `omitempty` constraint

--- logging.go
+++ logging.go
@@ -9,7 +9,7 @@ type LoggingConfig struct {
 	RelLogFileDir          string `json:"rel_log_file_dir" validate:"required"`
 	LogFileMaxBackups      int    `json:"log_file_max_backups" validate:"min=0"`
 	LogFileMaxAgeDays      int    `json:"log_file_max_age_days" validate:"min=0"`
-	LogFileMaxSizeMB       int    `json:"log_file_max_size_mb" validate:"min=1"`
+	LogFileMaxSizeMB       int    `json:"log_file_max_size_mb" validate:"omitempty,min=1"`
 	ShutdownTimeoutMS      int    `json:"shutdown_timeout_ms" validate:"omitempty,min=10,max=10000"` // Timeout for graceful shutdown (10ms-10s, 0=use default)
 	ShutdownTimeoutWarning bool   `json:"shutdown_timeout_warning"`                                  // Log warning if the shutdown timeout is exceeded
 
