3fe225eae75fb40f6ae5b5f017b4019b0b324e05
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-13 18:52:13 +0200

    Refactor user model and improve API key management
    Updated the `User` struct to include more detailed typing and aligned field names. Reworked API key generation and validation, simplifying key structures and methods while improving test coverage. Added a password tool to hash secrets for better security practices.

--- user.go
+++ user.go
@@ -1,13 +1,15 @@
 package types
 
+import "time"
+
 type User struct {
-	ID                 int64  `json:"id"`
-	Name               string `json:"name"`
-	PassHash           string `json:"pass_hash"`
-	Issuer             string `json:"issuer,omitempty"`
-	Subject            string `json:"subject,omitempty"`
-	Email              string `json:"email,omitempty"`
-	BootstrapHash      string `json:"bootstrap_hash,omitempty"`
-	BootstrapExpiresAt string `json:"bootstrap_expires_at,omitempty"`
-	BootstrapUsedAt    string `json:"bootstrap_used_at,omitempty"`
+	ID                 int64     `json:"id"`
+	Username           string    `json:"username"`
+	PassHash           string    `json:"pass_hash"`
+	Issuer             string    `json:"issuer,omitempty"`
+	Subject            string    `json:"subject,omitempty"`
+	Email              string    `json:"email,omitempty"`
+	BootstrapHash      string    `json:"bootstrap_hash,omitempty"`
+	BootstrapExpiresAt time.Time `json:"bootstrap_expires_at,omitempty"`
+	BootstrapUsedAt    time.Time `json:"bootstrap_used_at,omitempty"`
 }
