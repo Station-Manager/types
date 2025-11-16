d286100eca24237e9096dd6cafe90d2287ad05b8
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-16 06:59:02 +0200

    Add `ServerConfig` struct and integrate it into `AppConfig`

--- app_config.go
+++ app_config.go
@@ -4,4 +4,5 @@ type AppConfig struct {
 	DatastoreConfig DatastoreConfig `json:"datastore_config"`
 	LoggingConfig   LoggingConfig   `json:"logging_config"`
 	RequiredConfigs RequiredConfigs `json:"required_configs"`
+	ServerConfig    ServerConfig    `json:"server_config"`
 }
--- server.go
+++ server.go
@@ -18,3 +18,15 @@ type PostRequest struct {
 	Action   RequestAction `json:"action"`   // The action to perform
 	Data     string        `json:"data"`     // The data to send
 }
+
+type ServerConfig struct {
+	Name         string `json:"name"` // AppName for goFiber
+	Host         string `json:"host"`
+	Port         int    `json:"port"`
+	TLSEnabled   bool   `json:"tls_enabled"`
+	TLSCertFile  string `json:"tls_cert_file"`
+	TLSKeyFile   string `json:"tls_key_file"`
+	ReadTimeout  int    `json:"read_timeout"`  // Seconds
+	WriteTimeout int    `json:"write_timeout"` // Seconds
+	IdleTimeout  int    `json:"idle_timeout"`  // Seconds
+}
