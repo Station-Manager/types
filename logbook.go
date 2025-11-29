1aa990f5790871c6ef15a0539a2a8349303276bb
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-29 14:04:14 +0200

    Remove `omitempty` from Logbook.ID JSON tag to ensure ID is always included in the response

--- logbook.go
+++ logbook.go
@@ -1,7 +1,7 @@
 package types
 
 type Logbook struct {
-	ID          int64  `json:"id,omitempty"`
+	ID          int64  `json:"id"`
 	UserID      int64  `json:"user_id"`  // FK to users.id
 	Name        string `json:"name"`     // Unique name (to the user) for the logbook
 	Callsign    string `json:"callsign"` // The callsign associated with the logbook
