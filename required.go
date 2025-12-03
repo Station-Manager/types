9da5d0b9cbc02558a41e980efc2d2b23ac5d1718
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-12-03 12:25:08 +0200

    Update `PowerMultiplier` type in `RequiredConfigs` from `string` to `int`

--- required.go
+++ required.go
@@ -4,7 +4,7 @@ type RequiredConfigs struct {
 	DefaultRigID       int64  `json:"default_rig_id"`
 	DefaultFreq        string `json:"default_freq"`
 	DefaultMode        string `json:"default_mode"`
-	PowerMultiplier    string `json:"power_multiplier"`
+	PowerMultiplier    int    `json:"power_multiplier"`
 	OwnersCallsign     string `json:"owners_callsign"`
 	DefaultIsRandomQso bool   `json:"default_is_random_qso"`
 	DefaultTxPower     int    `json:"default_tx_power"`
