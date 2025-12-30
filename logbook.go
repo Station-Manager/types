2666a7747544c71e1d72a4e1bf1fc3ea1b3b8a31
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-12-30 07:52:39 +0200

    Mark `UserID` and `APIKey` fields in `Logbook` struct as `omitempty` to enhance JSON marshaling

--- logbook.go
+++ logbook.go
@@ -2,9 +2,9 @@ package types
 
 type Logbook struct {
 	ID          int64  `json:"id"`
-	UserID      int64  `json:"user_id"`  // FK to users.id
-	Name        string `json:"name"`     // Unique name (to the user) for the logbook
-	Callsign    string `json:"callsign"` // The callsign associated with the logbook
+	UserID      int64  `json:"user_id,omitempty"` // FK to users.id - only for postgres
+	Name        string `json:"name"`              // Unique name (to the user) for the logbook
+	Callsign    string `json:"callsign"`          // The callsign associated with the logbook
 	APIKey      string `json:"api_key,omitempty"`
 	Description string `json:"description,omitempty"`
 }
