d9924b5ab366095d61bb32c100982721e776ae45
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2026-01-03 13:57:35 +0200

    Rename `HttpTimeout` to `HttpTimeoutSec` in `ForwarderConfig` struct.

--- forwarding.go
+++ forwarding.go
@@ -3,12 +3,12 @@ package types
 import "time"
 
 type ForwarderConfig struct {
-	Name        string        `json:"name"`
-	Enabled     bool          `json:"enabled"`
-	URL         string        `json:"url"`
-	APIKey      string        `json:"apikey,omitempty"`
-	Username    string        `json:"username,omitempty"`
-	Password    string        `json:"password,omitempty"`
-	UserAgent   string        `json:"useragent"`
-	HttpTimeout time.Duration `json:"timeout"`
+	Name           string        `json:"name"`
+	Enabled        bool          `json:"enabled"`
+	URL            string        `json:"url"`
+	APIKey         string        `json:"apikey,omitempty"`
+	Username       string        `json:"username,omitempty"`
+	Password       string        `json:"password,omitempty"`
+	UserAgent      string        `json:"useragent"`
+	HttpTimeoutSec time.Duration `json:"timeout_sec"`
 }
