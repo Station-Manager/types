c986cff9e74b5296149ed0becf8fcb412bfa5558
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2026-01-03 14:00:52 +0200

    Rename `HttpTimeout` to `HttpTimeoutSec` in `LookupConfig` struct.

--- lookup.go
+++ lookup.go
@@ -3,12 +3,12 @@ package types
 import "time"
 
 type LookupConfig struct {
-	Name        string        `json:"name"`
-	Enabled     bool          `json:"enabled"`
-	URL         string        `json:"url"`
-	Username    string        `json:"username,omitempty"`
-	Password    string        `json:"password,omitempty"`
-	UserAgent   string        `json:"useragent"`
-	HttpTimeout time.Duration `json:"timeout"` // Seconds
-	ViewUrl     string        `json:"view_url,omitempty"`
+	Name           string        `json:"name"`
+	Enabled        bool          `json:"enabled"`
+	URL            string        `json:"url"`
+	Username       string        `json:"username,omitempty"`
+	Password       string        `json:"password,omitempty"`
+	UserAgent      string        `json:"useragent"`
+	HttpTimeoutSec time.Duration `json:"timeout_sec"` // Seconds
+	ViewUrl        string        `json:"view_url,omitempty"`
 }
